368e63242d1d0bbb03ff1ed887a5744da58d52b0
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2021-09-15 11:52:28 +0900

    연산자

---
+++ js_practice/practiceday6.js
@@ -0,0 +1,22 @@
+var x = 1;
+var y = 2;
+
+function plus(){
+    console.log("x+y=", x+y);
+}
+
+function minus(){
+    console.log("x-y", x-y);
+}
+
+function multiple(){
+    console.log("x*y", x*y);
+}
+
+function div(){
+    console.log("x/y=",x/y);
+}
+
+function por(){
+    console.log("x%y=", x%y);
+}
