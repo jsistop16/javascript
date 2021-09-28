642a474f1e86957312fcca20b27cfef13374d408
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2021-09-28 23:44:17 +0900

    코드 수정

--- js_practice/practiceday11.js
+++ js_practice/practiceday11.js
@@ -14,14 +14,30 @@ var dial = [
     {
         go:"안녕",
         ans:"안녕 반가워~"
+    },
+    {
+        go:"불꺼줘",
+        ans:"딸깍"
     }
+
 ]
 
 function check_text() {
     var con = document.getElementById("console").value;
     for(i=0; i<dial.length; i++){
+
+
+        if (dial[i] == "불꺼줘") {
+            document.getElementById("back").style.backgroundColor = "black";
+        } 
+        else {
+            document.body.style.backgroundColor = "white";
+        }
+
         if(dial[i].go==con){
             console.log(dial[i].ans);
         }
+
+
     }
 }
