4b3828472274a2d5d8922f9f7c4014e063196a3f
jsistop16 <83804417+jsistop16@users.noreply.github.com>
2021-09-29 13:47:41 +0900

    chat-bot 여러기능 추가(if)

--- js_practice/practiceday11.js
+++ js_practice/practiceday11.js
@@ -1,4 +1,4 @@
-var dial = [
+var dial = [ /*dial.length = 5*/
     {
         go:"이름이 뭐야?",
         ans:"심바"
@@ -22,22 +22,62 @@ var dial = [
 
 ]
 
+var k = 0;
+var cnt = 0;
+
 function check_text() {
+
     var con = document.getElementById("console").value;
+    var says = document.getElementsByClassName("says");
+
     for(i=0; i<dial.length; i++){
-
-
-        if (dial[i] == "불꺼줘") {
-            document.getElementById("back").style.backgroundColor = "black";
-        } 
-        else {
-            document.body.style.backgroundColor = "white";
-        }
-
         if(dial[i].go==con){
-            console.log(dial[i].ans);
+            says[0].innerHTML=dial[i].ans;
+            document.getElementById("back").style.backgroundColor="white";
+            k+=1;
+            if(con=="불꺼줘"){
+                document.getElementById("back").style.backgroundColor="grey";            
+            }
+            cnt++;
         }
+        
+    }
+        
+    if (cnt==0){
+        if(con == "불꺼도 나 보여?"){
+            says[0].innerHTML = "나는 보인다ㅎㅎ";
+            document.getElementById("back").style.backgroundColor="grey";
+            k+=1;
+        }
+        else if(con == "거짓말~"){
+            says[0].innerHTML = "나는 밤에도 잘보여";
+            k+=1;
+        }
+        else if(con == "내가 지금 뭐하고 있게?"){
+            says[0].innerHTML = "밥먹고있네?";
+            document.getElementById("back").style.backgroundColor="grey";
+            k+=1;
+        }
+        else if (con == "잘자~"){
+            says[0].innerHTML = "너도 잘자~";
+            k+=1;
+        }
+        else{
+            says[0].innerHTML = "뭐라고?";
+            document.getElementById("back").style.backgroundColor="white";
+            k+=1;
+        }
+    }
 
-
+    if (k==4){
+        var btn = document.getElementsByClassName("button");
+        btn[0].addEventListener("click", stop);
+    }
+    
+    function stop(){
+        setTimeout(function(){
+            says[0].innerHTML="나는 5번까지만 대답할수있어...";
+        }, 2000);
     }
 }
+
